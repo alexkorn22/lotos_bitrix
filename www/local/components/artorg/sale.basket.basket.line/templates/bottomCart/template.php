203b80267f0dc2aae555ed1702a38aa5a3f5a619
Denis <denigud@ya.ru>
2017-08-02 16:31:19 +0300

    - доработан вывод надписи "Доставка беспалтно" - надпись не выводится если в корзине нет товаров

--- www/local/components/artorg/sale.basket.basket.line/templates/bottomCart/template.php
+++ www/local/components/artorg/sale.basket.basket.line/templates/bottomCart/template.php
@@ -8,6 +8,6 @@ $frame = $this->createFrame()->begin();
 <div class="item">
 
 	<a <?if(!empty($arResult["NUM_PRODUCTS"])):?>href="<?=SITE_DIR?>personal/cart/"<?endif;?> class="cart<?if(!empty($arResult["NUM_PRODUCTS"])):?> active<?endif;?>"><span class="icon"></span><?=GetMessage("CART_LABEL")?><span class="mark"><?=$arResult["NUM_PRODUCTS"]?></span></a>
-    <span class="freeDelivery<?if($arResult["ART_TOTAL_PRICE"]<=0):?> mark<?endif;?>"><?if($arResult["ART_TOTAL_PRICE"]>0):?>До бесплатной доставки осталось <span class="mark"><?=$arResult["ART_TOTAL_PRICE_STR"];?></span><?else:?>Доставка бесплатно<?;endif;?></span>
+    <?if(!empty($arResult["NUM_PRODUCTS"])):?><span class="freeDelivery<?if($arResult["ART_TOTAL_PRICE"]<=0):?> mark<?endif;?>"><?if($arResult["ART_TOTAL_PRICE"]>0):?>До бесплатной доставки осталось <span class="mark"><?=$arResult["ART_TOTAL_PRICE_STR"];?></span><?else:?>Доставка бесплатно<?;endif;?></span><?endif;?>
 </div>
 <?$frame->end();?>
