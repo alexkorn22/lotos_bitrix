3635cb56465947527511d6bb81a1fa393af3c228
Denis <denigud@ya.ru>
2017-08-11 15:53:10 +0300

    - Мини корзина - доработан стиль карусели, доработан стиль надписи "Бесплатная доставка";

--- www/local/templates/dresscodeV2/components/bitrix/sale.basket.basket.line/bottomCart/template.php
+++ www/local/templates/dresscodeV2/components/bitrix/sale.basket.basket.line/bottomCart/template.php
@@ -8,7 +8,7 @@ $frame = $this->createFrame()->begin();
 <?if ($arParams["SHOW_PRODUCTS"] == "Y" && $arResult['NUM_PRODUCTS'] > 0):?>
 
     <div class="delivery-container<?if($arResult["ART_TOTAL_PRICE"]<=0):?> freeDelivery<?endif;?>">
-        <?if(!empty($arResult["NUM_PRODUCTS"]) && $arResult["FREE_DELIVERY_SUM"] > 0):?><span <?if($arResult["ART_TOTAL_PRICE"]<=0):?>class="mark"<?endif;?>><?if($arResult["ART_TOTAL_PRICE"]>0):?>До бесплатной доставки осталось <span class="mark"><?=$arResult["ART_TOTAL_PRICE_STR"];?></span><?else:?>Доставка бесплатно<?;endif;?></span><?endif;?>
+        <?if(!empty($arResult["NUM_PRODUCTS"]) && $arResult["FREE_DELIVERY_SUM"] > 0):?><span <?if($arResult["ART_TOTAL_PRICE"]<=0):?>class="mark"<?endif;?>><?if($arResult["ART_TOTAL_PRICE"]>0):?>До бесплатной доставки <span class="freeDeliveryLeftStr">осталось </span><?else:?>Доставка бесплатно<?;endif;?></span><span class="mark"><?if($arResult["ART_TOTAL_PRICE"]>0):?><?=$arResult["ART_TOTAL_PRICE_STR"];?><?endif;?></span><?endif;?>
         <!--<span style="font-size: 14px;">Доставка вашого замовлення можлива за 1 коп.</span>-->
     </div>
 
@@ -18,7 +18,7 @@ $frame = $this->createFrame()->begin();
                 <button type="button" data-role="none" class="button-arrow slick-prev-arrow">Previous</button>
                 <div id="miniCartFooterCarousel">
                     <? $n = 1; foreach ($arResult["CATEGORIES"]["READY"] as &$arItem):?>
-                        <div id="carouselProduct_<?=$arItem["ID"]?>" class="carousel-item">
+                        <div id="carouselProduct_<?=$arItem["ID"]?>" class="carousel-item" data-slick-index="<?=$n?>">
                             <img id="img-preview" class="product-image"
                                  src="<?=$arItem["PICTURE_SRC"]?>"
                                  alt="<?=ltrim($arItem["NAME"])?>"
@@ -42,46 +42,14 @@ $frame = $this->createFrame()->begin();
 
 <script type="text/javascript">
     $(document).ready(function(){
-        $('#miniCartFooterCarousel').slick({
+        $("#miniCartFooterCarousel").slick({
             appendArrows: $(".button-arrow"),
             prevArrow: $(".slick-prev-arrow"),
             nextArrow: $(".slick-next-arrow"),
-            accessibility:true,
-
             infinite: false,
-            speed: 300,
-            slidesToShow: 9,
+            slidesToShow: 5,
             slidesToScroll: 1,
-            responsive: [
-                {
-                    breakpoint: 1290,
-                    settings: {
-                        slidesToShow: 8,
-                        slidesToScroll: 1
-                    }
-                },
-                {
-                    breakpoint: 920,
-                    settings: {
-                        slidesToShow: 6,
-                        slidesToScroll: 1
-                    }
-                },
-                {
-                    breakpoint: 710,
-                    settings: {
-                        slidesToShow: 5,
-                        slidesToScroll: 1
-                    }
-                },
-                {
-                    breakpoint: 620,
-                    settings: {
-                        slidesToShow: 4,
-                        slidesToScroll: 1
-                    }
-                }
-            ]
+            speed: 300
         });
     });
 </script>
