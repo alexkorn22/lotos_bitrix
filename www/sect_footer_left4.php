7fed96b1e03b5d9db5ca52190e8a1a4b46b3bc16
Denis <denigud@ya.ru>
2017-08-19 15:37:25 +0300

    - Заполнена контактная информация

--- www/sect_footer_left4.php
+++ www/sect_footer_left4.php
@@ -1,10 +1,9 @@
 <div class="telephone footer_right">+380(61)214-99-54</div>
 <div class="telephone footer_right">+380(67)620-22-44</div>
-<div class="email footer_right">Эл. почта:</br> <a href="mailto:info@elta-market.com.ua">info@elta-market.com.ua</a></div>
-<div class="email footer_right">Отдел продаж:</br><a href="mailto:info@order@elta-market.com.ua">order@elta-market.com.ua</a></div>
+<div class="email footer_right">Эл. почта:</br> <a href="mailto:info@elta-market.com.ua">info@lotos-trade.ua</a></div>
+<div class="email footer_right">Отдел продаж:</br><a href="mailto:info@order@elta-market.com.ua">order@lotos-trade.ua</a></div>
 <ul class="list">
 	<li>График работы</li>
 	<li>Пн.-Пт. с 08:00 до 17:00</li>
-	<li>Сб: Выходной</li>
-	<li>Вс: Выходной</li>
+	<li>Сб, Вс : Выходной</li>
 </ul>
