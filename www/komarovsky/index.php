2ad8268c8f5c7fb12a48adfd8802933cad00dbaf
Denis <denigud@ya.ru>
2017-08-21 13:37:25 +0300

    - Добавлена страница хитов продаж

--- www/komarovsky/index.php
+++
@@ -1,4 +0,0 @@
-<?
-require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
-$APPLICATION->SetTitle("Title");
-?><pre><span style="color: #00a650; font-size: 36pt;">Наборы от Комаровского</span></pre><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
