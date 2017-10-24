e56e9d9f68e10b83cecea04b0490d8c1e78f5577
sergeydem <s.demjanenko@artorg.com.ua>
2017-10-24 12:46:57 +0300

    футер поправил. + слайдер динамический на разных разрешениях.

--- www/local/templates/dresscodeV2/components/bitrix/catalog.section/squares/template.php
+++ www/local/templates/dresscodeV2/components/bitrix/catalog.section/squares/template.php
@@ -67,14 +67,14 @@ $ds = new DataStore();
 								<?endif;?>
 							</a>
 						<?endif;?>
-						<a href="#" class="addCart<?if($arElement["CAN_BUY"] === false || $arElement["CAN_BUY"] === "N"):?> disabled<?endif;?>" data-id="<?=$arElement["ID"]?>"><img src="<?=SITE_TEMPLATE_PATH?>/images/incart.png" alt="" class="icon"><?=GetMessage("ADDCART_LABEL")?></a>
+						<a href="#" class="addCart<?if($arElement["CAN_BUY"] === false || $arElement["CAN_BUY"] === "N"):?> disabled<?endif;?>" data-id="<?=$arElement["ID"]?>"><i class="fa fa-shopping-bag new-bag" aria-hidden="true"></i><?=GetMessage("ADDCART_LABEL")?></a>
 					<?else:?>
 						<a class="price"><?=GetMessage("REQUEST_PRICE_LABEL")?></a>
 						<a href="#" class="addCart disabled requestPrice" data-id="<?=$arElement["ID"]?>"><img src="<?=SITE_TEMPLATE_PATH?>/images/request.png" alt="" class="icon"><?=GetMessage("REQUEST_PRICE_BUTTON_LABEL")?></a>
 					<?endif;?>
 					<div class="optional">
 						<div class="row">
-							<a href="#" class="fastBack label<?if(empty($arElement["MIN_PRICE"]) || $arElement["CAN_BUY"] === "N" || $arElement["CAN_BUY"] === false):?> disabled<?endif;?>" data-id="<?=$arElement["ID"]?>"><img src="<?=SITE_TEMPLATE_PATH?>/images/fastBack.png" alt="" class="icon"><?=GetMessage("FASTBACK_LABEL")?></a>
+							<a href="#" class="fastBack label<?if(empty($arElement["MIN_PRICE"]) || $arElement["CAN_BUY"] === "N" || $arElement["CAN_BUY"] === false):?> disabled<?endif;?>" data-id="<?=$arElement["ID"]?>"><i class="fa fa-shopping-bag new-bag" aria-hidden="true"></i><?=GetMessage("FASTBACK_LABEL")?></a>
 						</div>
 						<div class="row">
 
