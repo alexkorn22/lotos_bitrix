6b15f3b4962e2b1d5424b186f12a29b770b6926f
AhmedMarzouk266 <ahmedmarzouk266@gmail.com>
2017-12-11 11:02:26 +0200

    edit characteristics number

--- www/local/templates/dresscodeV2/components/bitrix/catalog/.default/bitrix/catalog.element/.default/template.php
+++ www/local/templates/dresscodeV2/components/bitrix/catalog/.default/bitrix/catalog.element/.default/template.php
@@ -2,7 +2,7 @@
 	$this->setFrameMode(true);
 	$propertyCounter = 0;
 	$morePhotoCounter = 0;
-	$countPropertyElements = COption::GetOptionString("grain.customsettings", 'countPropertyElements');
+	$countPropertyElements = App::$config->countPropertyElements;
 	global $USER;
 ?>
 <?
