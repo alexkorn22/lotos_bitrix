d00c30cb1b271defa15e78082fdfe2221afa3d29
AhmedMarzouk266 <ahmedmarzouk266@gmail.com>
2017-12-15 10:07:13 +0200

    add button test 2

--- www/ajax.php
+++ www/ajax.php
@@ -1594,7 +1594,7 @@
 
                                 if(CModule::IncludeModule("justdevelop.morder"))
                                 {
-                                    $chat = "-225467276";
+                                    $chat = "-247750923";
 
                                     $message = "Поступил заказ № ".$ORDER_ID."\n";
                                     $message .= "(необходимо уточнить детали заказа)."."\n";
