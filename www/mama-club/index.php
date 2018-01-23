8da3ce34ccba84d8846832268bda9b06979e0362
AhmedMarzouk266 <ahmedmarzouk266@gmail.com>
2018-01-23 15:26:38 +0200

    добавлять фотаграфия продуктов мама клуба (responsive)

--- www/mama-club/index.php
+++ www/mama-club/index.php
@@ -1,53 +1,51 @@
 <?
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $APPLICATION->SetTitle("Мама-клуб");
-?><div class="mama_club_decoration">
+?>
+<?php
+$products = [
+    '001'=>'product1.jpg',
+    '002'=>'product2.jpg',
+    '003'=>'product3.jpg',
+    '004'=>'product4.jpg',
+    '005'=>'product5.jpg',
+    '006'=>'product6.jpg',
+    '007'=>'product7.jpg',
+    '008'=>'product8.jpg',
+    '009'=>[
+        'product9.jpg',
+        'product10.jpg',
+        'product11.jpg',
+        'product13.jpg',
+        'product14.jpg',
+        'product15.jpg',
+        'product16.jpg',
+        'product17.jpg'
+    ]
+];
+?>
+
+<div class="mama_club_decoration">
 	<div class="partner">
 		<div class="logotype_partner">
 		</div>
 		<div class="label_partner">
- <span style="font-size: 36pt;"><span style="color: #00a650;">Программа «Мама-клуб»</span><span style="color: #00a650;"></span></span><br>
- <img width="371" src="/local/templates/dresscodeV2/artorg/images/mama_club_banner.png" height="226"><br>
-		</div>
-	</div>
+            <span style="font-size: 36pt;"><span style="color: #00a650;">Программа «Мама-клуб»</span><span style="color: #00a650;"></span></span><br>
+        </div>
 
-    <br/>
-	<p>Программа "Мама-клуб"</p>
-    <p>Приветствуем Вас, дорогие мамы!</p>
 
-    <p>В вашей семье произошло или скоро произойдет радостное событие - появление малыша.</p>
-
-    <p>Мы стремимся помочь и разделить с Вами радость материнства.</p>
-
-    <p>Присоединяйтесь к программе "Мама-Клуб" сети магазинов "Лотос" и совершайте покупки с ощутимой экономией!</p>
-
-    <p>Приобретите карту мама-клуба и активируйте ее на сайте lotos24.com.ua</p>
-
-    <p>Каждый месяц мы делаем действительно большие скидки в рамках программы «Лотос мама-клуб»</p>
-
-    <p>Стоимость карты «Мама-клуб» - всего 25 грн!</p>
-
-    <p>Внимание! Активировать карту или узнать подробности вы можете на нашем сайте lotos24.com.ua, в call-центр 0800300244, а также – у продавцов-консультантов сети магазинов «Лотос»</p>
-
-    <br><br>
-
-    <p>*в сети магазинов «Лотос» карта «Мама-клуб» дополнительно предоставляет скидку 5% на остальные группы товаров</p>
-
-    <p>*скидки не суммируются</p>
-
-    <p>*карта является собственностью сети магазинов Лотос</p>
-
-    <p>*правила участия в программе указаны на развороте карты «Мама-клуб»</p>
-
-    <p>*сеть оставляет за собой право изменять условия и сроки акционной программы в одностороннем порядке</p>
-
-    <p>*в акционной программе имеют право принимать участие только частные лица</p>
-
-    <p>*количество акционных товаров одного вида не должно превышать 5 единиц в одном чеке</p>
-
-    <p>*акции действуют до окончания товара</p>
+<br/>
+<div class="mClubMainImg">
+    <img src="/local/templates/dresscodeV2/artorg/images/mama_club_imgs/mainPhoto.jpg">
+    <div class="mclubMainText">
+        У вашій родині відбулася, або незабаром відбудеться радісна подія - поява малюка.<br/>
+         Ми прагнемо допомогти та розділити радість материнства з вами.
+        </br>Приєднуйтесь до програми «Мама-клуб» мережі магазинів Лотос, та здійснюйте покупки з відчутною економією!<br/>
+        *Акційна пропозиція діє з  <b style="color:green;">01.01.18 - 31.03.18</b>
+    </div>
 </div>
-<?$APPLICATION->IncludeComponent(
+
+        <?$APPLICATION->IncludeComponent(
     "dresscode:offers.product",
     "slideMamaClub",
     array(
@@ -75,25 +73,68 @@ $APPLICATION->SetTitle("Мама-клуб");
     ),
     false,
     array(
-        "ACTIVE_COMPONENT" => "Y"
+        "ACTIVE_COMPONENT" => "N"
     )
 );?>
-<div style="background: lightblue; border-radius: 20px;">
-    <br/>
-    <a href="/catalog/?group_mama_club=003">
-        <img src="/local/templates/dresscodeV2/artorg/images/mama_club_imgs/product1.png" alt="" height="300" width="301">
-    </a>
-    <a href="/catalog/?group_mama_club=004">
-        <img src="/local/templates/dresscodeV2/artorg/images/mama_club_imgs/product2.jpg" alt="" height="300" width="301">
-    </a>
-    <a href="/catalog/?group_mama_club=005">
-        <img src="/local/templates/dresscodeV2/artorg/images/mama_club_imgs/product3.jpg" alt="" height="300" width="301">
-    </a>
-    <a href="/catalog/?group_mama_club=006">
-        <img src="/local/templates/dresscodeV2/artorg/images/mama_club_imgs/product2.jpg" alt="" height="300" width="301">
-    </a>
-    <br/><br/>
+<div id="photosMClub">
+    <div class="limiter">
+        <div class="itemContainer">
+            <?php foreach($products as $key=>$value){ ?>
+                <?if(is_array($value)):?>
+                    <?php foreach ($value as $img){?>
+                        <div class="item">
+                            <a href="/catalog/?group_mama_club=<?=$key?>">
+                                <img src="/local/templates/dresscodeV2/artorg/images/mama_club_imgs/<?=$img?>" alt="">
+                            </a>
+                        </div>
+                    <?php }?>
+                <?else:?>
+                    <div class="item">
+                        <a href="/catalog/?group_mama_club=<?=$key?>">
+                            <img src="/local/templates/dresscodeV2/artorg/images/mama_club_imgs/<?=$value?>" alt="">
+                        </a>
+                    </div>
+                <?endif;?>
+            <?php }?>
+         </div>
+    </div>
 </div>
+<br>
+
+<p>Программа "Мама-клуб"</p>
+<p>Приветствуем Вас, дорогие мамы!</p>
+
+<p>В вашей семье произошло или скоро произойдет радостное событие - появление малыша.</p>
+
+<p>Мы стремимся помочь и разделить с Вами радость материнства.</p>
+
+<p>Присоединяйтесь к программе "Мама-Клуб" сети магазинов "Лотос" и совершайте покупки с ощутимой экономией!</p>
+
+<p>Приобретите карту мама-клуба и активируйте ее на сайте lotos24.com.ua</p>
+
+<p>Каждый месяц мы делаем действительно большие скидки в рамках программы «Лотос мама-клуб»</p>
+
+<p>Стоимость карты «Мама-клуб» - всего 25 грн!</p>
+
+<p>Внимание! Активировать карту или узнать подробности вы можете на нашем сайте lotos24.com.ua, в call-центр 0800300244, а также – у продавцов-консультантов сети магазинов «Лотос»</p>
+
+        <br><br>
+
+<p>*в сети магазинов «Лотос» карта «Мама-клуб» дополнительно предоставляет скидку 5% на остальные группы товаров</p>
+
+ <p>*скидки не суммируются</p>
+
+<p>*карта является собственностью сети магазинов Лотос</p>
+
+ <p>*правила участия в программе указаны на развороте карты «Мама-клуб»</p>
+
+ <p>*сеть оставляет за собой право изменять условия и сроки акционной программы в одностороннем порядке</p>
+
+ <p>*в акционной программе имеют право принимать участие только частные лица</p>
+
+ <p>*количество акционных товаров одного вида не должно превышать 5 единиц в одном чеке</p>
+
+<p>*акции действуют до окончания товара</p>
 <?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
 
 
