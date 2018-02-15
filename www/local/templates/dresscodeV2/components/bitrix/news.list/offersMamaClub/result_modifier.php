384b64180c955a1196b572acacd9dafaa0ebf6c1
AhmedMarzouk266 <AhmedMarzouk266@gmail.com>
2018-02-15 16:16:57 +0200

    добавить result_modifier.php 2

--- www/local/templates/dresscodeV2/components/bitrix/news.list/offersMamaClub/result_modifier.php
+++ www/local/templates/dresscodeV2/components/bitrix/news.list/offersMamaClub/result_modifier.php
@@ -1,16 +1,16 @@
 <?php
 
 
-foreach ($arResult["ITEMS"] as $arElement) {
+foreach ($arResult["ITEMS"] as $key => $arElement) {
     $image = CFile::ResizeImageGet($arElement["PREVIEW_PICTURE"], array("width" => 430, "height" => 250), BX_RESIZE_IMAGE_PROPORTIONAL, false);
-    $arResult[$arElement['ID']]['imageSrc'] = $image['src'];
+    $arResult['ITEMS'][$key]['imageSrc'] = $image['src'];
     $db_props = CIBlockElement::GetProperty($arParams['IBLOCK_ID'], $arElement["ID"], "sort", "asc", array());
     while ($ar_props = $db_props->Fetch()) {
         if ($ar_props['CODE'] == 'group_mama_club') {
-            $arResult[$arElement['ID']]['groupMamaClub'] = $ar_props['VALUE'];
+            $arResult['ITEMS'][$key]['groupMamaClub'] = $ar_props['VALUE'];
         }
         if ($ar_props['CODE'] == 'sale') {
-            $arResult[$arElement['ID']]['sale'] = $ar_props['VALUE'];
+            $arResult['ITEMS'][$key]['sale'] = $ar_props['VALUE'];
         }
     }
 }
--- www/local/templates/dresscodeV2/components/bitrix/news.list/offersMamaClub/template.php
+++ www/local/templates/dresscodeV2/components/bitrix/news.list/offersMamaClub/template.php
@@ -3,26 +3,22 @@
 ?>
 <div class="photosMClub">
     <div class="itemContainer">
-            <?foreach ($arResult["ITEMS"] as $arElement):
-                    $groupMamaClub = $arResult[$arElement['ID']]['groupMamaClub'];
-                    $sale          = $arResult[$arElement['ID']]['sale'] ;
-                    $imageSrc      = $arResult[$arElement['ID']]['imageSrc'] ;
-                ?>
+            <?foreach ($arResult["ITEMS"] as $arElement):?>
                 <div class="item">
                     <div class="tabloid">
-                        <a href="/catalog/?group_mama_club=<?=$groupMamaClub?>" class="picture">
-                            <img src="<?=$imageSrc?>" alt="">
+                        <a href="/catalog/?group_mama_club=<?=$arElement['groupMamaClub']?>" class="picture">
+                            <img src="<?=$arElement['imageSrc']?>" alt="">
                         </a>
                         <div class="title">
-                            <a href="/catalog/?group_mama_club=<?=$groupMamaClub?>" class="name">
+                            <a href="/catalog/?group_mama_club=<?=$arElement['groupMamaClub']?>" class="name">
                                 <span><?=$arElement["NAME"]?></span>
                             </a>
                         </div>
                         <div class="sale">
-                            <span><?=$sale?></span>
+                            <span><?=$arElement['sale']?></span>
                         </div>
                         <div class="details-item">
-                            <a href="/catalog/?group_mama_club=<?=$groupMamaClub?>" class="details-lnk">
+                            <a href="/catalog/?group_mama_club=<?=$arElement['groupMamaClub']?>" class="details-lnk">
                                 Узнать подробности
                             </a>
                         </div>
